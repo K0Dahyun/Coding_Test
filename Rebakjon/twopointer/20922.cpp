68f3ead1f20802e01cd76b28bff2095a860ba1d9
K0Dahyun <gdh1685@kookmin.ac.kr>
2024-08-02 11:45:17 +0900

    20922

---
+++ Rebakjon/twopointer/20922.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> v(n, 0);
+    vector<int> count(200001, 0);
+
+    int start = 0;
+    int max_length = 0;
+
+    for(int end = 0; end < n; end++) {
+        cin >> v[end];
+        count[v[end]]++;
+
+        while(count[v[end]] > k)
+            count[v[start++]]--;
+
+        max_length = max(end - start + 1, max_length);
+    }
+
+
+    cout << max_length << endl;
+
+    return 0;
+}
