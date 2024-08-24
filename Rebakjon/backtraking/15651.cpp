45af99b175f613d8e597eea6643e86beecd2525c
K0Dahyun <gdh1685@kookmin.ac.kr>
2024-08-24 21:50:00 +0900

    15651

---
+++ Rebakjon/backtraking/15651.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+vector<int> arr;
+vector<bool> visited;
+
+void dfs(int depth) {
+    if(depth == m) {
+        for(int i = 0; i < m; i++)
+            cout << arr[i] <<" ";
+        cout << "\n";
+        return;
+    }
+
+    for(int i = 1; i <= n; i++) {
+        arr[depth] = i;
+        visited[i] = true;
+        dfs(depth + 1);
+        visited[i] = false;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+    arr.resize(m, 0);
+    visited.resize(m, 0);
+
+    dfs(0);
+}
